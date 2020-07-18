9e1d15d48d77ccf10bdb0c60e58d910e8ca3a256
peter rosanelli <peter.rosanelli@gmail.com>
2020-07-18 16:17:36 -0400

    map http => https

--- src/Map.js
+++ src/Map.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const Map = props => (
   <div className="card">
     <div className="card-body">
-      <img src={`http://maps.googleapis.com/maps/api/staticmap?center=${props.location}&zoom=${props.zoom}&size=350x250&markers=color:red%7C${props.location}&sensor=false&key=AIzaSyBC8ydXBc7Uo78yhuUUXHUXcsfsf3phhmE`} alt="map" className="img-fluid" />
+      <img src={`https://maps.googleapis.com/maps/api/staticmap?center=${props.location}&zoom=${props.zoom}&size=350x250&markers=color:red%7C${props.location}&sensor=false&key=AIzaSyBC8ydXBc7Uo78yhuUUXHUXcsfsf3phhmE`} alt="map" className="img-fluid" />
     </div>
   </div>
 )
